bad538515c0f603bd2747e948fcd99a8fd18c404
Patrick Wendell <pwendell@gmail.com>
2014-02-19 18:08:03 -0800

    Removing VERSION env var.
    Eearlier I thought we could use this to create nonce'd versions so that
    we don't deal with confusing caching behavior in ivy. We can maybe do
    that later but for now I just find it annoying.

--- build.sbt
+++ build.sbt
@@ -3,12 +3,14 @@ import com.typesafe.tools.mima.plugin.MimaPlugin.mimaDefaultSettings
 
 mimaDefaultSettings
 
-previousArtifact := Some("mima-tester" % "mima-tester_2.10" % System.getenv.get("VERSION"))
+scalaVersion := "2.10.3"
+
+previousArtifact := Some("mima-tester" % "mima-tester_2.10" % "1.0")
 
 name := "Mima Tester"
 
-version := System.getenv.get("VERSION")
+version := "1.0"
 
-scalaVersion := "2.10.3"
+libraryDependencies += "org.scala-lang" % "scalap" % "2.10.3"
 
 scalaSource in Compile := baseDirectory.value / System.getenv.get("SOURCE_DIR")
--- run-tests.sh
+++ run-tests.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-# Can be automated later to run many tests
+# Can be automated later to run specific tests, output nicely, etc.
 
-VERSION=1.0 SOURCE_DIR=private-inner-class/before/ sbt publish-local
-VERSION=1.0 SOURCE_DIR=private-inner-class/after/ sbt mima-report-binary-issues
+SOURCE_DIR=private-inner-class/before/ sbt publish-local
+SOURCE_DIR=private-inner-class/after/ sbt mima-report-binary-issues
 
-VERSION=1.0 SOURCE_DIR=package-private-class/before/ sbt publish-local
-VERSION=1.0 SOURCE_DIR=package-private-class/after/ sbt mima-report-binary-issues
+SOURCE_DIR=package-private-class/before/ sbt publish-local
+SOURCE_DIR=package-private-class/after/ sbt mima-report-binary-issues
